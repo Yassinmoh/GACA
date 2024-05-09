c8c836381da7f92b644fb6dd45a7415216638966
Yassinmoh <yassin_mohamed310.com>
2024-05-10 01:09:24 +0300

    Feat(App): Create Header and intro component

--- src/app/modules/home/pages/home-page/home-page.component.ts
+++ src/app/modules/home/pages/home-page/home-page.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 // import { NgxIntlTelInputModule } from 'ngx-intl-tel-input/lib/ngx-intl-tel-input.module';
+import {ButtonModule} from 'primeng/button'
+import { IntroComponent } from '../../components/intro/intro.component';
+
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule,IntroComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
